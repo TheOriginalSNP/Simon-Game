ce4f4745b69b8460c6cade66eed0bfedb455761b
Ivan Martinez <51198881+TheOriginalSNP@users.noreply.github.com>
2023-03-08 09:54:50 -0500

    Working game, no repeat sequence

--- game.js
+++ game.js
@@ -34,7 +34,12 @@ function checkAnswer(currentLevel) {
             }, 1000);
         }
     } else {
-        console.log("Wrong");
+        $("body").addClass("game-over")
+        setTimeout(function () {
+            $("body").removeClass("game-over")
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver()
     }
 }
 
@@ -59,4 +64,10 @@ function animatePress(currentColor) {
     setTimeout(function () {
         $("#" + currentColor).removeClass("pressed")
     }, 100);
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    gameStarted = false;
 }
