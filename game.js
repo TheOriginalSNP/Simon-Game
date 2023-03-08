44d1387c13d457f9eeddc83fb207ae85532dfada
Ivan Martinez <51198881+TheOriginalSNP@users.noreply.github.com>
2023-03-08 10:22:19 -0500

    add sequence repeater

--- game.js
+++ game.js
@@ -52,8 +52,12 @@ function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
-    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColor);
+    for (let i = 0; i < gamePattern.length; i++) {
+        setTimeout(function () {
+            $("#" + gamePattern[i]).fadeIn(100).fadeOut(100).fadeIn(100);
+            playSound(gamePattern[i]);
+        }, i * 500);
+    }
 }
 
 function playSound(name) {
